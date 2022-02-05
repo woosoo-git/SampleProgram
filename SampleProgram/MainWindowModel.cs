ba9e8b0eaf477905886f7a0a2f24bf69624ef8a9
DESKTOP-A367UOP\charm <charmingtalker@gmail.com>
2022-02-05 18:04:44 +0900

    초기 커밋.

---
+++ SampleProgram/MainWindowModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleProgram
+{
+    public class MainWindowModel
+    {
+        public string ProgramTitle => $"Sample Test V{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+
+    }
+}
